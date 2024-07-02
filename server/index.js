05d47a76f29417b63102b18face36f9259391acc
satvikB7 <148987999+satvikB7@users.noreply.github.com>
2024-07-02 21:35:10 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,13 @@ dotenv.config();
 
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST","GET","PATCH","DELETE","PUT"],
+    credentails: true
+  }
+));
 
 app.get('/', (req, res) => {
   res.send('This is a stack overflow clone API');
