d042713ba55ab603e2e7c130496c24f410eb2d2b
satvikB7 <148987999+satvikB7@users.noreply.github.com>
2024-07-03 20:01:06 +0530

    Update chat.js

--- client/src/api/chat.js
+++ client/src/api/chat.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://stack-overflow-wine-three.vercel.app/',
   headers: {
     'Content-Type': 'application/json',
   },
