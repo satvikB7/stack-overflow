43db6a610e43d10fbc756d9c55b54144213bd111
satvikB7 <148987999+satvikB7@users.noreply.github.com>
2024-07-03 21:24:14 +0530

    Update ChatPage.jsx

--- client/src/components/ChatRoom/ChatPage.jsx
+++ client/src/components/ChatRoom/ChatPage.jsx
@@ -18,7 +18,7 @@ const ChatPage = () => {
   const User = useSelector((state) => state.currentUserReducer);
 
   useEffect(() => {
-    socketRef.current = io('https://stack-overflow-wine-three.vercel.app/', {
+    socketRef.current = io('stack-overflow-client-seven.vercel.app/', {
       transports: ['websocket'], 
     });
     const socket = socketRef.current;
