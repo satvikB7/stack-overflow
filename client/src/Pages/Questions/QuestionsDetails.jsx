ac413fc4d96a684694f64907b1f28e2f807aa59a
satvikB7 <148987999+satvikB7@users.noreply.github.com>
2024-07-03 20:11:03 +0530

    Update QuestionsDetails.jsx

--- client/src/Pages/Questions/QuestionsDetails.jsx
+++ client/src/Pages/Questions/QuestionsDetails.jsx
@@ -75,7 +75,7 @@ const QuestionsDetails = () => {
     const dispatch = useDispatch()
     const User = useSelector((state) => (state.currentUserReducer))
     const location = useLocation()
-    const url = 'http://localhost:3000'
+    const url = 'https://stack-overflow-client-seven.vercel.app/'
 
     const handlePostAns = (e, answerLength) => {
         e.preventDefault() //avoids reloading the page
